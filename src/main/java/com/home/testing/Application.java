6a55398056f9d74827eb9de949909a4d1b397ea5
Marijana <cvetmarijana@gmail.com>
2020-05-21 01:03:09 +0200

    Adding Application Java source

---
+++ src/main/java/com/home/testing/Application.java
@@ -0,0 +1,14 @@
+package com.home.testing;
+
+public class Application {
+    
+    public Application() {
+        System.out.println ("Hello world!F");
+    }
+
+   
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+	    Application app = new Application();
+    }
+}
