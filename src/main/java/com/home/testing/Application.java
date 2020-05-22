51ebcf97bf282fd042bd88649fea18c66cf6f1ae
Marijana <cvetmarijana@gmail.com>
2020-05-22 21:27:01 +0200

    StringUtils and added dependency to lang3

--- src/main/java/com/home/testing/Application.java
+++ src/main/java/com/home/testing/Application.java
@@ -1,14 +1,26 @@
 package com.home.testing;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+	
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+		}
     
     public Application() {
-        System.out.println ("Hello world!F");
+       System.out.println ("Hello world!F");
+		
+		
     }
 
    
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
+		//app.greet();
+		int count = app.countWords("Creating my first maven project");
+		System.out.println("Word count: " + count);
     }
 }
